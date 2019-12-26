ef2272680b15edb540d82af3c490784c0f035abd
Vladislav Moiseev <v.moiseev@aisgorod.ru>
2019-12-26 20:28:34 +0400

    added IEsiaSigner. need to test it

---
+++ src/EsiaSigner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AISGorod.AspNetCore.Authentication.Esia
+{
+    /// <summary>
+    /// Интерфейс для подписи данных с помощью ключа ИС.
+    /// </summary>
+    public interface IEsiaSigner
+    {
+        /// <summary>
+        /// Подписать последовательность байт при помощи ключа из сертификата ИС.
+        /// </summary>
+        /// <param name="data">Данные для подписи.</param>
+        /// <returns>Откреплённая подпись.</returns>
+        string Sign(byte[] data);
+    }
+}
